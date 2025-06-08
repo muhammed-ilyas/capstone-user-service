93318440c14cee149641505afdb97133a37567a7
SMI240 <shaik.ilyas@maersk.com>
2025-06-08 13:59:13 +0530

    initial commit

---
+++ src/main/java/com/aim/capstoneuserservice/CapstoneUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aim.capstoneuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneUserServiceApplication.class, args);
+    }
+
+}
